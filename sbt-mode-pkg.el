723bf5f607c359bd19585d72dbe64eb1358e365b
Heikki Vesalainen <heikkivesalainen@yahoo.com>
2013-11-11 13:18:41 +0200

    add dependency on scala-mode2

--- sbt-mode-pkg.el
+++ sbt-mode-pkg.el
@@ -1,2 +1,4 @@
-(define-package "sbt-mode" "0.01" "Major mode for sbt >= 0.12 with scala >= 2.9")
+(define-package "sbt-mode" "0.02" 
+  "Major mode for sbt >= 0.12 with scala >= 2.9"
+  '((scala-mode "0.22")))
 
