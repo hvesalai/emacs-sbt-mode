9ab672e7db024b3b22d45d2f80d83787d98e5b39
Andreas Koestler <andreas.koestler@gmail.com>
2014-09-22 14:36:45 +1000

    (sbt:command "console") is non-blocking; subsequent comint-send-region calls will have no effect until the console is up. It is more consistent to require the console to be up and produce an error message if it is not instead of firing it up in the same command.

--- sbt-mode-comint.el
+++ sbt-mode-comint.el
@@ -222,8 +222,13 @@ line.")
              "No sbt or scala prompt found before process mark")))))
 
 (defun sbt:send-region (start end)
-  (unless (comint-check-proc (sbt:buffer-name))
-    (error "sbt is not running in buffer %s" (sbt:buffer-name)))
+  (let ((submode (buffer-local-value 'sbt:submode
+                                     (get-buffer (sbt:buffer-name)))))
+    (message "submode %s" submode)
+    (unless (and (comint-check-proc (sbt:buffer-name))
+                 (or  (eq submode 'console)
+                      (eq submode 'paste-mode)))
+      (error "sbt console is not running in buffer %s" (sbt:buffer-name))))
   (save-excursion
     (goto-char end)
     (skip-syntax-forward ">")
@@ -235,19 +240,8 @@ line.")
     (setq start (point)))
   (unless (> end start) (error "mark a region of code first"))
   (display-buffer (sbt:buffer-name))
-  (let ((submode (buffer-local-value 'sbt:submode
-                                     (get-buffer (sbt:buffer-name)))))
-    (unless (or (eq submode 'console) (eq submode 'paste-mode))
-      (sbt-command "console")))
-  ;; TODO: Do not send region if there is an error.
-  ;;
-  ;; There may be compilation by (sbt-command "console")
-  (let ((submode (buffer-local-value 'sbt:submode
-                                     (get-buffer (sbt:buffer-name)))))
-    (message "submode: %s" submode)
-    (when (or (eq submode 'console) (eq submode 'paste-mode))
-      (comint-send-region (sbt:buffer-name) start end)
-      (comint-send-string (sbt:buffer-name) "\n"))))
+  (comint-send-region (sbt:buffer-name) start end)
+  (comint-send-string (sbt:buffer-name) "\n"))
 
 
 (defun sbt:paste-region (start end &optional no-exit)
